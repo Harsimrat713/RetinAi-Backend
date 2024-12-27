f04ffbe0dfbd3a01da49710f8ebfcf51579df8f3
Harsimrat Bajwa <harsimrat713@gmail.com>
2024-12-26 18:53:53 -0700

    Add non eye test image, updated utilities to save and delete images, added format images function

--- utilities.py
+++ utilities.py
@@ -1,7 +1,37 @@
-import uuid
+import uuid, os
+
+imagePaths = ['session_images/recieved_images', 'session_images/formatted_images', 'session_images/cropped_images']
 
 def genKey():
     # generate key for data base
     key = str(uuid.uuid4())
     return key
 
+async def localImageSave(images):
+    # open all images and return a list of all the image names
+    imageList = []
+    for image in images:
+        # print for testing
+        print(f"Received image: {image.filename}, Content-Type: {image.content_type}")
+        imageList.append(image.filename)
+        # save images to session images
+        contents = await image.read()
+        with open(f"session_images/recieved_images/uploaded_{image.filename}", "wb") as f:
+            f.write(contents)
+    return imageList
+
+def deleteSessionImages(paths):
+    # delete all images that were used for this session
+    for path in paths:
+        dir_list = os.listdir(path)
+        for dir in dir_list:
+            os.remove(f"{path}/{dir}")
+    print("deleted")
+    return
+
+# for testing
+def main():
+    deleteSessionImages(imagePaths)
+
+if __name__ == '__main__':
+    main()
